231b35947f9d97e9dc495cf9f84622ae77dd4772
Safarmurod999 <supermen0904@gmail.com>
2023-02-13 14:54:10 +0500

    main

--- js/card.js
+++ js/card.js
@@ -13,7 +13,7 @@ let stockObject = [
     discount: "../images/notice.png",
     cardPrice: "44,50 ₽",
     defPrice: "50,50 ₽",
-    description: "Молоко ПРОСТОКВАШИНО паст вареные,...",
+    description: "Г/Ц Блинчики c мясом вес,вареные, Россия",
   },
   {
     img: "../images/stock-3.png",
@@ -21,7 +21,7 @@ let stockObject = [
     discount: "../images/notice.png",
     cardPrice: "44,50 ₽",
     defPrice: "50,50 ₽",
-    description: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ...",
+    description: "Г/Ц Блинчики c мясом вес,вареные, Россия",
   },
   {
     img: "../images/stock-4.png",
@@ -29,7 +29,7 @@ let stockObject = [
     discount: "../images/notice.png",
     cardPrice: "44,50 ₽",
     defPrice: "50,50 ₽",
-    description: "Сосиски вареные МЯСНАЯ ИСТОРИЯ...",
+    description: "Г/Ц Блинчики c мясом вес,вареные, Россия",
   },
 ];
 
@@ -263,4 +263,70 @@ function getSpecCard(title, text, src) {
 for (el of specObject) {
   let card = getSpecCard(el.title, el.text, el.src);
   specRow.append(card);
+}
+
+let statObject = [
+  {
+    img: "../images/status-1.png",
+    title: "Режим использования масок и перчаток на территории магазинов",
+    description:
+      'Подробная информация o режимах использования масок и перчаток на территории магазинов "ЛЕНТА". Информация обновляется.',
+  },
+  {
+    img: "../images/status-2.png",
+    title: "Весеннее настроение для каждой",
+    description:
+      "8 Марта - это не просто Международный женский день, это ещё день тюльпанов, приятных сюрпризов и праздничных тёплых пожеланий.\n",
+  },
+  {
+    img: "../images/status-3.png",
+    title: "ЗОЖ или ФАСТФУД. A вы на чьей стороне? Голосуем!",
+    description:
+      "Голосуйте за любимые категории, выбирайте категорию-победителя в мобильном приложении и получайте кешбэк 10% баллами в апреле!",
+  },
+];
+
+let statusRow = document.getElementById("statusRow");
+
+function getStatus(img, title, description) {
+  let statusCard = document.createElement("div");
+  let mainStatus = document.createElement("div");
+  mainStatus.className = "col-12";
+  mainStatus.classList.add("col-lg-4");
+  mainStatus.classList.add("mt-3");
+  mainStatus.classList.add("mt-lg-0");
+
+  let status_body=document.createElement("div");
+
+  statusCard.classList.add("status__card");
+  let status_img = document.createElement("img");
+  status_img.src = img;
+  statusCard.append(status_img);
+
+  let status_date = document.createElement("h5");
+  status_date.innerHTML = "05.03.2021";
+  status_body.append(status_date);
+
+  let status_title = document.createElement("h4");
+  status_title.innerHTML = title;
+  status_body.append(status_title);
+
+  let status_description = document.createElement("p");
+  status_description.innerHTML = description;
+  status_body.append(status_description);
+
+  let status_btn = document.createElement("button");
+  status_btn.innerHTML = "Подробнее";
+  status_body.append(status_btn);
+
+  statusCard.append(status_body);
+
+  mainStatus.append(statusCard);
+
+  return mainStatus;
+}
+
+for (el of statObject) {
+  let card = getStatus(el.img, el.title, el.description);
+  statusRow.append(card);
 }
